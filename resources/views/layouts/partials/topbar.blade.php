27514f3bc90aa1d0c3948be239ed96fa47cac3b7
miarta <miarta.igede@gmail.com>
2021-11-13 22:42:49 +0800

    staging

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -9,26 +9,15 @@
 
         <div class="right-topbar ml-auto">
             <ul class="navbar-nav">
-                {{-- <li class="nav-item name-mobile ">
-                    <div class="d-flex justify-content-end">
-                        <div class="media-body user-mobile">
-                            <p class="user-name mb-0 ">{{ auth()->user()->fullname }}</p>
-                            <p class="designattion mb-0 text-success">
-                                {{ auth()->user()->level }}</p>
-                        </div>
-                    </div>
-                </li> --}}
 
-                <a class="nav-link dropdown-toggle dropdown-toggle-nocaret user-mobile mr-n3" href="javascript:;"
-                    data-toggle="dropdown">
-                    <div class="media user-box align-items-center ">
-                        <div class="media-body  ">
-                            <p class="user-name mb-0 ">{{ auth()->user()->fullname }}</p>
-                            <p class="designattion mb-0 text-success">
-                                {{ auth()->user()->level }}</p>
-                        </div>
+                <li class="nav-item name-mobile ">
+                    <div class="d-flex justify-content-end">
+                        <h6 class="user-name mt-3 mb-0 justify-content-end">{{ auth()->user()->fullname }} </h6>
+
+                        <p class="designattion mt-2  ml-n5 mb-0 text-success">{{ strtoupper(auth()->user()->level) }}
+                        </p>
                     </div>
-                </a>
+                </li>
 
                 {{-- account login --}}
                 <li class="nav-item dropdown dropdown-user-profile">
@@ -38,7 +27,7 @@
                             <div class="media-body user-info">
                                 <p class="user-name mb-0 ">{{ auth()->user()->fullname }}</p>
                                 <p class="designattion mb-0 text-success">
-                                    {{ auth()->user()->level }}</p>
+                                    {{ strtoupper(auth()->user()->level) }}</p>
                             </div>
                             <img src="https://via.placeholder.com/110x110" class="user-img" alt="user avatar">
                         </div>
