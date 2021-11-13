c5b4bbd6560e1b2f65afd322075bff68d8c366ba
miarta <miarta.igede@gmail.com>
2021-11-13 21:58:26 +0800

    css topbar v2

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -9,15 +9,26 @@
 
         <div class="right-topbar ml-auto">
             <ul class="navbar-nav">
-
-                <li class="nav-item name-mobile ">
-                    <div class="d-flex justify-content-end user-info">
-                        <h6 class="user-name mt-3 mb-0 justify-content-end">{{ auth()->user()->fullname }} </h6>
-
-                        <p class="designattion mt-2  ml-n5 mb-0 text-success">{{ auth()->user()->level }}
-                        </p>
+                {{-- <li class="nav-item name-mobile ">
+                    <div class="d-flex justify-content-end">
+                        <div class="media-body user-mobile">
+                            <p class="user-name mb-0 ">{{ auth()->user()->fullname }}</p>
+                            <p class="designattion mb-0 text-success">
+                                {{ auth()->user()->level }}</p>
+                        </div>
                     </div>
-                </li>
+                </li> --}}
+
+                <a class="nav-link dropdown-toggle dropdown-toggle-nocaret user-mobile mr-n3" href="javascript:;"
+                    data-toggle="dropdown">
+                    <div class="media user-box align-items-center ">
+                        <div class="media-body  ">
+                            <p class="user-name mb-0 ">{{ auth()->user()->fullname }}</p>
+                            <p class="designattion mb-0 text-success">
+                                {{ auth()->user()->level }}</p>
+                        </div>
+                    </div>
+                </a>
 
                 {{-- account login --}}
                 <li class="nav-item dropdown dropdown-user-profile">
