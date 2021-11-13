93da074dc05eaed140048e419de31ff9cbc0e4d6
miarta <miarta.igede@gmail.com>
2021-11-13 10:42:37 +0800

    C:/Program Files/Git/home

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/login');
 });
+Route::get('/home', function () {
+    return redirect('/dashboard');
+});
 Route::get('/login', [LoginController::class,'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class,'authenticate']);
 Route::get('/register', [LoginController::class,'regist'])->middleware('guest');
